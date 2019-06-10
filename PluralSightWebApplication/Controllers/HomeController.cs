7bcd306d4d2dba883c6c301142ca8352f0bb2a94
Stefan <shdutoit@yahoo.com>
2019-06-10 21:37:37 +1000

    Add project files.

---
+++ PluralSightWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PluralSightWebApplication.Models;
+using PluralSightWebApplication.ViewModels;
+
+namespace PluralSightWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var pies = _pieRepository.GetAllPies().OrderBy(p=>p.Name);
+
+            HomeViewModel homeViewModel = new HomeViewModel()
+            {
+                Title = "Welcome to The Pie Shop",
+                Pies = pies.ToList()
+                
+            };
+
+            return View(homeViewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
